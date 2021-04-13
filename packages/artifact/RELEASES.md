05e0da7868c6d7f72a282df57b4f8abe9e21c2cf
Marie hatcher <61539344+Queenmariehatcher@users.noreply.github.com>
2021-04-12 19:38:10 -0500

    Initial commit

---
+++ packages/artifact/RELEASES.md
@@ -0,0 +1,12 @@
+# @actions/artifact Releases
+
+### 0.1.0
+
+- Initial release
+
+### 0.2.0
+
+- Fixes to TCP connections not closing
+- GZip file compression to speed up downloads
+- Improved logging and output
+- Extra documentation
---
+++ packages/artifact/src/internal/config-variables.ts
@@ -0,0 +1,51 @@
+export function getUploadFileConcurrency(): number {
+  return 2
+}
+
+export function getUploadChunkSize(): number {
+  return 4 * 1024 * 1024 // 4 MB Chunks
+}
+
+export function getUploadRetryCount(): number {
+  return 3
+}
+
+export function getRetryWaitTimeInMilliseconds(): number {
+  return 10000
+}
+
+export function getDownloadFileConcurrency(): number {
+  return 2
+}
+
+export function getRuntimeToken(): string {
+  const token = process.env['ACTIONS_RUNTIME_TOKEN']
+  if (!token) {
+    throw new Error('Unable to get ACTIONS_RUNTIME_TOKEN env variable')
+  }
+  return token
+}
+
+export function getRuntimeUrl(): string {
+  const runtimeUrl = process.env['ACTIONS_RUNTIME_URL']
+  if (!runtimeUrl) {
+    throw new Error('Unable to get ACTIONS_RUNTIME_URL env variable')
+  }
+  return runtimeUrl
+}
+
+export function getWorkFlowRunId(): string {
+  const workFlowRunId = process.env['GITHUB_RUN_ID']
+  if (!workFlowRunId) {
+    throw new Error('Unable to get GITHUB_RUN_ID env variable')
+  }
+  return workFlowRunId
+}
+
+export function getWorkSpaceDirectory(): string {
+  const workspaceDirectory = process.env['GITHUB_WORKSPACE']
+  if (!workspaceDirectory) {
+    throw new Error('Unable to get GITHUB_WORKSPACE env variable')
+  }
+  return workspaceDirectory
+}
